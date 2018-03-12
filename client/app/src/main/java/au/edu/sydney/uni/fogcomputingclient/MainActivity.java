7f1efa43c40f3d8a5439bc08b2ef9a7274bb3167
cshe6391 <cshe6391@pc-4e55-0.it.usyd.edu.au>
2018-03-12 16:31:41 +1100

    init the android project

---
+++ client/app/src/main/java/au/edu/sydney/uni/fogcomputingclient/MainActivity.java
@@ -0,0 +1,18 @@
+package au.edu.sydney.uni.fogcomputingclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (null == savedInstanceState) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, CameraFragment.newInstance())
+                    .commit();
+        }
+    }
+}
